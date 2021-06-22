0b27980c1999c085cca20d936acd45965eeb9428
tworang89 <86291425+tworang89@users.noreply.github.com>
2021-06-22 18:04:56 +0900

    Add files via upload

---
+++ day01/src/start/Hi.java
@@ -0,0 +1,13 @@
+package start;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("안녕");
+		System.out.println("반가워");
+		System.out.println("잘가");
+		// TODO Auto-generated method stub
+
+	}
+
+}
